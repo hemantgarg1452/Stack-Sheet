844b8a1ca4f8bd811c6693f0339b85cdb6c65b20
Hemant garg <108003083+hemantgarg1452@users.noreply.github.com>
2023-05-28 16:00:28 +0530

    Add files via upload

---
+++ qs1.cpp
@@ -0,0 +1,56 @@
+// You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+// Evaluate the expression. Return an integer that represents the value of the expression.
+
+// Note that:
+
+// The valid operators are '+', '-', '*', and '/'.
+// Each operand may be an integer or another expression.
+// The division between two integers always truncates toward zero.
+// There will not be any division by zero.
+// The input represents a valid arithmetic expression in a reverse polish notation.
+// The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ 
+
+// Example 1:
+
+// Input: tokens = ["2","1","+","3","*"]
+// Output: 9
+// Explanation: ((2 + 1) * 3) = 9
+#include<bits/stdc++.h>
+#include<vector>
+#include<stack>
+using namespace std;
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int>st;
+        for(auto x:tokens){
+            if(x == "+" || x == "-" || x == "*" || x == "/"){
+                int op2 = st.top(); st.pop();
+                int op1 = st.top(); st.pop();
+                if(x == "+"){
+                    st.push(op1 + op2);
+                }
+                if(x == "-"){
+                    st.push(op1 - op2);
+                }
+                if(x == "*"){
+                    st.push(op1 * op2);
+                }
+                if(x == "/"){
+                    st.push(op1 / op2);
+                }
+            }
+            else{
+                stringstream ss(x);
+                int data;
+                ss>>data;
+                st.push(data);
+
+            }
+        }
+        return st.top();
+    }
+};
