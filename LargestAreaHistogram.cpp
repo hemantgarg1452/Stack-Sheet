0897784e48dbdbd5c6ecac867484e06bd682ebe1
Hemant garg <108003083+hemantgarg1452@users.noreply.github.com>
2023-09-17 14:18:09 +0530

    Add files via upload

---
+++ LargestAreaHistogram.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int res=0;
+        int n = heights.size();
+        vector<int>ps = prev_smaller(heights);
+        vector<int>ns = next_smaller(heights);
+
+        for(int i=0; i<n; i++){
+            int curr = (ns[i]-ps[i]-1) * heights[i];
+            res = max(res, curr);
+        }
+        return res;
+
+    }
+
+    vector<int>prev_smaller(vector<int>& heights){
+        vector<int>ans;
+        stack<int>st;
+
+        int n = heights.size();
+        for(int i=0; i<n; i++){
+            while(st.empty() == false && heights[st.top()]>= heights[i]){
+                st.pop();
+            }
+            int ele = (st.empty()) ? -1 : st.top();
+            ans.push_back(ele);
+            st.push(i);
+        }
+        //reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+    vector<int>next_smaller(vector<int>& heights){
+        vector<int>ans;
+        stack<int>st;
+        int n = heights.size();
+
+        for(int i=n-1; i>=0; i--){
+            while(st.empty() == false && heights[st.top()]>=heights[i]){
+                st.pop();
+            }
+            int ele = (st.empty()) ? n : st.top();
+            ans.push_back(ele);
+            st.push(i);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
